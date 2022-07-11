3d55d14aa8b2aa46c2515cae7a8a3eb019a35e50
gastonsj <74213975+gastonsj@users.noreply.github.com>
2022-07-11 19:53:08 -0300

    Boolean and
    Define a function named triple_and that takes three parameters and returns True only if they are all True and False otherwise.

---
+++ exercise_18.py
@@ -0,0 +1,8 @@
+"""
+https://pythonprinciples.com/challenges/Boolean-and/
+Boolean and
+
+Define a function named triple_and that takes three parameters and returns True only if they are all True and False otherwise.
+"""
+def triple_and(a,b,c):
+    return a & b & c
