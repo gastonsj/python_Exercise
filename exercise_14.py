f8094503d968dae92da9d477b2a8a0c5bb6b0f58
gastonsj <74213975+gastonsj@users.noreply.github.com>
2022-07-11 19:00:04 -0300

    Palindrome
    A string is a palindrome when it is the same when read backwards.
    
    For example, the string "bob" is a palindrome. So is "abba". But the string "abcd" is not a palindrome, because "abcd" != "dcba".
    
    Write a function named palindrome that takes a single string as its parameter. Your function should return True if the string is

---
+++ exercise_14.py
@@ -0,0 +1,15 @@
+"""
+https://pythonprinciples.com/challenges/Palindrome/
+
+Palindrome
+
+A string is a palindrome when it is the same when read backwards.
+
+For example, the string "bob" is a palindrome. So is "abba". But the string "abcd" is not a palindrome, because "abcd" != "dcba".
+
+Write a function named palindrome that takes a single string as its parameter. Your function should return True if the string is a palindrome, and False otherwise.
+"""
+def palindrome (str):
+    return str == str[::-1]
+#TEST
+print(palindrome("bob"))
