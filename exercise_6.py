3080e318462a3db56d103e640dd85780bf35f81f
gastonsj <74213975+gastonsj@users.noreply.github.com>
2022-07-11 11:22:25 -0300

    Double letters
    The goal of this challenge is to analyze a string to check if it contains two of the same letter in a row. For example, the string "hello" has l twice in a row, while the string "nono" does not have two identical letters in a row.
    
    Define a function named double_letters that takes a single parameter. The parameter is a string. Your function must return True if there are two identical letters in a row in the string, and False otherwise.

---
+++ exercise_6.py
@@ -0,0 +1,17 @@
+
+"""
+https://pythonprinciples.com/challenges/Double-letters/
+
+Double letters
+The goal of this challenge is to analyze a string to check if it contains two of the same letter in a row. For example, the string "hello" has l twice in a row, while the string "nono" does not have two identical letters in a row.
+
+Define a function named double_letters that takes a single parameter. The parameter is a string. Your function must return True if there are two identical letters in a row in the string, and False otherwise.
+"""
+def double_letters(str):
+    flag=False
+    for pos in range(len(str)-1):
+        if str[pos] == str[pos+1]:
+            return True
+    return False
+#TEST
+print(double_letters("Hello"))
