5a4d482abe6d8158f0d30e2347c1bba41ad2682a
gastonsj <74213975+gastonsj@users.noreply.github.com>
2022-07-11 11:12:26 -0300

    Type check
    Type check
    Write a function named only_ints that takes two parameters. Your function should return True if both parameters are integers, and False otherwise.
    
    For example, calling only_ints(1, 2) should return True, while calling only_ints("a", 1) should return False.

--- exercise_5.py
+++ exercise_5.py
@@ -7,10 +7,8 @@ Write a function named only_ints that takes two parameters. Your function should
 For example, calling only_ints(1, 2) should return True, while calling only_ints("a", 1) should return False.
 """
 def only_ints(a,b):
-    cond1= type(a) == type(b)
-    cond2= type(a) != type(True)
-    cond3= type(True) != type(b)
-    cond4= type(a) == type(1)
-    return cond1 & cond2 & cond3 & cond4
+    cond1= type(a) == int
+    cond2= type(b) == type(1)
+    return cond1 & cond2
 #TEST
 print(only_ints(1,2))
